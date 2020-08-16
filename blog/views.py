1929bc50acc4ed2a8048a12eb09894e38b5c35fe
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-16 19:13:51 -0300

    refactor views blog: include class HomeView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
 
 
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
