8a2dcf825e4f3f7c47c7459f86d0f7b629d06490
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-16 22:22:56 -0300

    change urls in blog: create path addPostView

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('artigo/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('novo_post/', AddPostView.as_view(), name="add-post")
 ]
