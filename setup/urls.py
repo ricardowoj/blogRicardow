9f22eda240d3103ef64634143b4779655cca7a67
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-17 17:54:31 -0300

    change urls in setup: create path to app members

--- setup/urls.py
+++ setup/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('usuario/', include('django.contrib.auth.urls')),
+    path('usuario/', include('members.urls')),
 ]
