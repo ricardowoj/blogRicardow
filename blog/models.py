3371abf4ab7b9fe43416c8da21c92e150363b924
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-17 15:24:09 -0300

    change models in blog: include post date class post

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-    @staticmethod
     def get_absolute_url(self):
         return reverse('home')
