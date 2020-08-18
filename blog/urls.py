37509d37cb450a9228e91f85cec2715807118139
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-17 22:00:46 -0300

    change urls in blog: create path name add-category

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, AddCategoryView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('artigo/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
     path('novo_post/', AddPostView.as_view(), name="add-post"),
+    path('nova_categoria/', AddCategoryView.as_view(), name="add-category"),
     path('artigo/editar/<int:pk>', UpdatePostView.as_view(), name="update-post"),
     path('artigo/deletar/<int:pk>', DeletePostView.as_view(), name="delete-post"),
 ]
