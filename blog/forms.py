ff7bd32e186c83b940db708e8489ebb302183f24
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-17 22:01:57 -0300

    change form in blog: create class CategoryForm and add category in class PostForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,24 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name',)
+        widgets = {
+            'category': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 
 class PostForm(forms.ModelForm):
     class Meta:
+        choices = Category.objects.all().values_list('name', 'name')
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'author', 'category', 'body')
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o título'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
         }
