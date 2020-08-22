4d3e4e2d3ed925c348309b715b7f7f633b72fecb
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-22 14:16:46 -0300

    change views in blog: add function like_view adn edit get_contest_data for total_likes

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import Post, Category
 from blog.forms import PostForm, CategoryForm
+
+
+def like_view(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
+
+def category_view(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats': cats.title(), 'category_posts': category_posts})
 
 
 class HomeView(ListView):
@@ -17,14 +29,19 @@ class HomeView(ListView):
         return context
 
 
-def category_view(request, cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats': cats.title(), 'category_posts': category_posts})
-
-
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        categories_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context["categories_menu"] = categories_menu
+        context["total_likes"] = total_likes
+        return context
 
 
 class AddPostView(CreateView):
