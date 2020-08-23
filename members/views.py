da693cd199a5b03e7302d1045f2bde6ac5d12380
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-23 02:08:02 -0300

    change views in members: add class UserEditView

--- members/views.py
+++ members/views.py
@@ -1,9 +1,18 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from members.forms import SignUpForm
+from members.forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = './registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = './registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
