b38eff0351f6e60c23d4445bf2c2f13126a390b7
ricardowoj <ricardowoj@hotmail.com>
2020-08-23 16:33:46 -0300

    change forms in members: add class EditProfileForm and PasswordChangingForm

--- members/forms.py
+++ members/forms.py
@@ -1,16 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(
+        max_length=100,
+        label="Nome",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(
+        max_length=100,
+        label="Sobrenome",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1',
+                  'password2')
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
@@ -20,11 +28,51 @@ class SignUpForm(UserCreationForm):
 
 
 class EditProfileForm(UserChangeForm):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    UserChangeForm.password = None
+    username = forms.CharField(
+        max_length=100,
+        label="Usuário",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(
+        max_length=100,
+        label="Nome",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(
+        max_length=100,
+        label="Sobrenome",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        max_length=100,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class PasswordChangingForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        max_length=100,
+        label="Senha atual",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'type': 'password'
+        }))
+    new_password1 = forms.CharField(
+        max_length=100,
+        label="Nova senha",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'type': 'password'
+        }))
+    new_password2 = forms.CharField(
+        max_length=100,
+        label="Confirme a senha",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'type': 'password'
+        }))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2')
