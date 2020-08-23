decadc29701ac19b471fd762bbafc297d8025d9c
ricardowoj <ricardowoj@hotmail.com>
2020-08-23 16:31:29 -0300

    change urls in members: add path name edit-password

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from members.views import UserRegisterView, UserEditView
-
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('cadastro/', UserRegisterView.as_view(), name='register'),
     path('editar-cadastro/', UserEditView.as_view(), name='edit-profile'),
+    path('password/', PasswordsChangeView.as_view(), name='edit-password'),
 ]
