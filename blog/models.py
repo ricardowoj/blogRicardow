53a9545abb58023a3ec4f0cd001aa25d136c10d4
ricardowoj <ricardowoj@hotmail.com>
2020-08-24 08:30:55 -0300

    change models in blog: add class profile

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,17 @@ class Category(models.Model):
         return reverse('home')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = models.ImageField(null=True,
+                                        blank=True,
+                                        upload_to="images/profile/")
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(null=True,
