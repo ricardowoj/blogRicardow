417bd66fd4956db6fe603d0d76278e4b5b101acf
ricardowoj <ricardowoj@hotmail.com>
2020-08-24 12:34:30 -0300

    refactor forms in members

--- members/forms.py
+++ members/forms.py
@@ -4,6 +4,10 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=100,
+        label="Usuário",
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(
         max_length=100,
         label="Nome",
@@ -14,17 +18,19 @@ class SignUpForm(UserCreationForm):
         widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(
         attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        max_length=100,
+        label="Senha",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(
+        max_length=100,
+        label="Confirmar senha",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1',
                   'password2')
-
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
 class EditProfileForm(UserChangeForm):
