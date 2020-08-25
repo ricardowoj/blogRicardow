13a504ef72f0d4938ec2ed09c524907824da6763
ricardowoj <ricardowoj@hotmail.com>
2020-08-25 18:06:26 -0300

    edit admin in members: add admin register profile

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('usuario/', include('django.contrib.auth.urls')),
     path('usuario/', include('members.urls')),
+    path('usuario/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
