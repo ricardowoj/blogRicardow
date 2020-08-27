b3f7401478ee8cdb7c20833d562b8e8c3cd12cc1
ricardowoj <ricardowoj@hotmail.com>
2020-08-26 23:15:48 -0300

    edit models in blog: add social media in class profile

--- blog/models.py
+++ blog/models.py
@@ -21,9 +21,11 @@ class Profile(models.Model):
                                         blank=True,
                                         upload_to="images/profile/")
     website_url = models.CharField(max_length=255, null=True, blank=True)
-    facebook = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
     twitter_url = models.CharField(max_length=255, null=True, blank=True)
     instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    github_url = models.CharField(max_length=255, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
